595668e600d5f22df4297ac21d3f65174618b57b
thieu85 <thieuhao2525@gmail.com>
2018-10-28 15:13:32 +0700

    main source

---
+++ app/Http/Controllers/EmailController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Jobs\SendEmailJob;
+
+class EmailController extends Controller
+{
+    public function sendEmail()
+    {
+    	dispatch(new SendEmailJob());
+    	echo "gửi email thành công";
+    }
+}
---
+++ resources/views/home/welcomeEmail.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h3 style="text-align: center">Welcome to my website!</h3>
+<h4 style="color: red"><a href="{{route('home.page')}}" title="">VietNamLestgo.info</a></h4>
+</body>
+</html>
